753936ccbe697cbd1f6a10de1de35b64c188672b
verandert <verandert@163.com>
2020-04-23 15:26:11 +0800

    主函数测试

--- main.c
+++ main.c
@@ -4,8 +4,11 @@ int main(){
     Elemtype data[] = "AB  CD  EF   ";//先序二叉树列表,以此构造二叉树;
     BiTreeNode *T;
     CreateBiTree(&T, data);
-    InOrderTraverse(T);
-    PreOrderTraverse(T);
-    PostOrderTraverse(T);
+    // InOrderTraverse(T);
+    // PreOrderTraverse(T);
+    // PostOrderTraverse(T);
+    printf("后序递归遍历二叉树:");
+    PostOrderTraverseV1(T);
+    printf("\n");
     return 0;
 }
